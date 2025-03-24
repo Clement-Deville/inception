62f9d5a3d46af78d8c09e106c7d8e1428453249b
leonardo <leonardo@cdeville.42.fr>
2025-03-24 15:58:00 +0100

    Configuration du websocket de goaccess pour qu'elle soit accessible depuis l'hote de la vm a l'adresse localhost:4443/goaccess/ws

--- srcs/bonus/goaccess/tools/run.sh
+++ srcs/bonus/goaccess/tools/run.sh
@@ -10,6 +10,5 @@ exec /usr/bin/goaccess -f /var/log/nginx/access.log \
           --real-time-html \
           -o /var/www/html/sample.com/goaccess/report.html --port=7890 \
           --config-file=/etc/goaccess/goaccess.conf --log-format=COMBINED \
-	  --ws-url=wss://localhost.cdeville.42.fr:4443/goaccess/ws \
-	  --origin=https://localhost.cdeville.42.fr:4443/goaccess
+	  --ws-url=wss://localhost:4443/goaccess/ws
 	
